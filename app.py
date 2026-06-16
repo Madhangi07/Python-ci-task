395638037a5919bbed72c0db9cc307622794ca83
Madhangi07 <madhangir2005@gmail.com>
2026-06-16 14:12:32 +0530

    Initial python ci task demo

---
+++ app.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    if n < 0:
+        raise ValueError("Factorial not defined for negative numbers")
+
+    if n == 0:
+        return 1
+
+    return n * factorial(n - 1)
---
+++ test_app.py
@@ -0,0 +1,33 @@
+from app import factorial
+import pytest
+
+def test_factorial_0():
+    assert factorial(0) == 1
+
+def test_factorial_1():
+    assert factorial(1) == 1
+
+def test_factorial_2():
+    assert factorial(2) == 2
+
+def test_factorial_3():
+    assert factorial(3) == 6
+
+def test_factorial_4():
+    assert factorial(4) == 24
+
+def test_factorial_5():
+    assert factorial(5) == 120
+
+def test_factorial_6():
+    assert factorial(6) == 720
+
+def test_factorial_7():
+    assert factorial(7) == 5040
+
+def test_factorial_8():
+    assert factorial(8) == 40320
+
+def test_factorial_negative():
+    with pytest.raises(ValueError):
+        factorial(-1)
